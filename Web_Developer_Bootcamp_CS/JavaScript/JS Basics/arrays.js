55a150388ef3906a392a49b39e4aefa72a2ce248
Mykola <mykola.maksymov@gmail.com>
2021-12-15 18:09:49 +0100

    start arrays

---
+++ Web_Developer_Bootcamp_CS/JavaScript/JS Basics/arrays.js
@@ -0,0 +1,16 @@
+let colors =['blue', 'green', 'black', 'white'];
+console.log(colors[0]);
+console.log(colors.length);
+const leaderboard = ['Harry', 'Lua', 'Hermione', 'Bellatrix']; //DON'T TOUCH THIS LINE!
+// YOUR CODE BELOW HERE:
+leaderboard[1] ='Luna';
+leaderboard[3] ='Draco';
+leaderboard.push('Bellatrix');
+leaderboard.push('Rone');
+leaderboard.pop();
+leaderboard.unshift('Mykola');
+leaderboard.shift();
+const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+planets.shift();
+planets.push('Saturn');
+planets.unshift('Mercury');
