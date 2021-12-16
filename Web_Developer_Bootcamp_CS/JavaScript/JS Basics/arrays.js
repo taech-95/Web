738485c43102c0a00dd7f8628e5042ffafc8d6d4
Mykola <mykola.maksymov@gmail.com>
2021-12-16 18:13:41 +0100

    finish arrays

--- Web_Developer_Bootcamp_CS/JavaScript/JS Basics/arrays.js
+++ Web_Developer_Bootcamp_CS/JavaScript/JS Basics/arrays.js
@@ -14,3 +14,20 @@ const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH
 planets.shift();
 planets.push('Saturn');
 planets.unshift('Mercury');
+
+let slicesPlanets = planets.slice(1,3);
+slicesPlanets.splice(1,0, "Pluton");
+let scores = [1, 6, 12, 87, 132, 22412421, -12]
+scores.sort();
+console.log(scores)
+const gameBoard = [['X', 'O', 'X'],['O', null, 'X'],['O', 'O', 'X']]
+console.log(gameBoard[1][1]);
+
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+airplaneSeats[3][1] = "Hugo";
