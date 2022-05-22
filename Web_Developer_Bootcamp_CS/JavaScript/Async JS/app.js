9faaf42e256f352da498767df3a3b32019c52ffc
Mykola <mykola.maksymov@gmail.com>
2022-05-22 14:29:35 +0200

    test branch

--- Web_Developer_Bootcamp_CS/JavaScript/Async JS/app.js
+++ Web_Developer_Bootcamp_CS/JavaScript/Async JS/app.js
@@ -37,4 +37,56 @@ sing
         console.log(err)
     })
 
+    const login = async (password, username) => {
+        if (!username || !password ) {
+            throw 'Missing Credentials';
+        }
+        if (password === 'corgifeeatarecute') {
+            return ' Welcome';
+        }
+        throw 'Invalid Password';
+    } 
+
+    login('asdasdsadas')
+    .then((msg)=>{
+        console.log('Correct ', msg)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+    const delayedColor = ((color, delay) =>{
+        return new Promise((resole, reject) => {
+            setTimeout(()=>{
+                document.body.style.backgroundColor = color;
+            }, delay)
+        });
+    });
     
+    
+    delayedColor('red', 2000)
+    .then (()=>{
+        delayedColor('orange', 2000)
+    })
+    .then (()=>{
+        delayedColor('blue', 2000)
+    })
+    .then (()=>{
+        delayedColor('vindigo', 2000)
+    })
+    .then (()=>{
+        delayedColor('green', 2000)
+    })
+
+    async function rainbow(){
+        await delayedColor('red', 1000);
+        await delayedColor('green', 1000);
+        await delayedColor('blue', 1000);
+        await delayedColor('vendigo', 1000);
+        await delayedColor('black', 1000);
+        await delayedColor('orange', 1000);
+    }
+
+    async function  showRaibow() {
+        await rainbow();
+        console.log("All done")
+    }
--- Web_Developer_Bootcamp_CS/JavaScript/TV Show project/app.js
+++ Web_Developer_Bootcamp_CS/JavaScript/TV Show project/app.js
@@ -1,105 +1,181 @@
 
 
-fetch('https://icanhazdadjoke.com/')
-    .then (res => {
-        console.log ("RESPONSE", res)
-        return res.json()
-    })
-    .then(data =>{
-        console.log('Data', data)
-    })
-    .catch(e=>{
-        console.log("Error", e)
-    })
+// fetch('https://icanhazdadjoke.com/')
+//     .then (res => {
+//         console.log ("RESPONSE", res)
+//         return res.json()
+//     })
+//     .then(data =>{
+//         console.log('Data', data)
+//     })
+//     .catch(e=>{
+//         console.log("Error", e)
+//     })
 
-const fetchBitPrice = async () => {
+// const fetchBitPrice = async () => {
 
-    try {
-        const res = await fetch('https://icanhazdadjoke.com/');
-        const data = await res.json;
-        console.log(data);
-    } catch (error) {
-        console.log(error);
-    }
+//     try {
+//         const res = await fetch('https://icanhazdadjoke.com/');
+//         const data = await res.json;
+//         console.log(data);
+//     } catch (error) {
+//         console.log(error);
+//     }
     
-}
+// }
 
-axios.get('https://icanhazdadjoke.com/')
-    .then(response => {
-        console.log(response)
-    })
-    .catch(er => {
-        console.log(err);
-    })
+// axios.get('https://icanhazdadjoke.com/')
+//     .then(response => {
+//         console.log(response)
+//     })
+//     .catch(er => {
+//         console.log(err);
+//     })
 
-const fetchBitPricev2 = async () =>{
-    try {
-        const res = await axios.get('https://icanhazdadjoke.com/')
-        console.log(res);
-    } catch (error) {
-        console.log(error)
-    }
-}
+// const fetchBitPricev2 = async () =>{
+//     try {
+//         const res = await axios.get('https://icanhazdadjoke.com/')
+//         console.log(res);
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
-const jokes = document.querySelector('#jokes');
-
-
-const getJoke = document.querySelector('button');
+// const jokes = document.querySelector('#jokes');
 
 
-
-const addNewJoke = async ()=>{
-    try {
-        const getJoke = await getDadJoke();
-        const newLi = document.createElement('LI');
-        newLi.append(getJoke)
-        jokes.append(newLi);
-    } catch (error) {
-        console.log('No Jokes avaliable')
-    }
-}
+// const getJoke = document.querySelector('button');
 
 
-const getDadJoke = async ()=> {
-    try {
-        const headers = {headers: {Accept: 'application/json'}}
-        const res = await axios.get('https://icanhazdadjoke.com/', headers);
-        // console.log(res.data.joke);
-        return res.data.joke;
-    } catch (error) {
-        console.log('API problems');
-    }
+
+// const addNewJoke = async ()=>{
+//     try {
+//         const getJoke = await getDadJoke();
+//         const newLi = document.createElement('LI');
+//         newLi.append(getJoke)
+//         jokes.append(newLi);
+//     } catch (error) {
+//         console.log('No Jokes avaliable')
+//     }
+// }
+
+
+// const getDadJoke = async ()=> {
+//     try {
+//         const headers = {headers: {Accept: 'application/json'}}
+//         const res = await axios.get('https://icanhazdadjoke.com/', headers);
+//         // console.log(res.data.joke);
+//         return res.data.joke;
+//     } catch (error) {
+//         console.log('API problems');
+//     }
    
-}
-getJoke.addEventListener('click', addNewJoke);
+// }
+// getJoke.addEventListener('click', addNewJoke);
 
-const searchForm = document.querySelector('#searchForm')
-searchForm.addEventListener('submit', async function(e) {
-    e.preventDefault();
-    console.dir(searchForm);
-    console.log(searchForm.elements.query.value);
-    const search = searchForm.elements.query.value;
-    try {
-        const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${search}`);
-        displayImages(res.data);
-        searchForm.elements.query.value='';
-        // console.log(res.data[0].show.image.medium);
-        // const img = document.createElement('img');
-        // img.src = res.data[0].show.image.medium;
-        // document.body.append(img)
-    } catch (error) {
-        console.log(error);
-    }
+// const searchForm = document.querySelector('#searchForm')
+// searchForm.addEventListener('submit', async function(e) {
+//     e.preventDefault();
+//     console.dir(searchForm);
+//     console.log(searchForm.elements.query.value);
+//     const search = searchForm.elements.query.value;
+//     try {
+//         const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${search}`);
+//         displayImages(res.data);
+//         searchForm.elements.query.value='';
+//         // console.log(res.data[0].show.image.medium);
+//         // const img = document.createElement('img');
+//         // img.src = res.data[0].show.image.medium;
+//         // document.body.append(img)
+//     } catch (error) {
+//         console.log(error);
+//     }
     
+// });
+
+
+// const displayImages  = (shows) => {
+//     for (let show of shows) {
+//         if (show.show.image) {
+//             const img = document.createElement('img');
+//             img.src = show.show.image.medium;
+//             document.body.append(img)
+//         }
+//     }
+// }
+
+
+// revisited 
+//promises 
+
+const fakeRequest = (url) => {
+    return new  Promise((resolve, reject)=> {
+        const rand = Math.random();
+        setTimeout(()=>{
+            if(rand < 0.7){
+                resolve('It worked!!!');
+            }
+            else {
+                reject('It\'s failed!!!');
+            }
+            
+        }, 1000)
+    })
+} 
+
+fakeRequest('/dogs1')
+    .then((data)=>{
+        console.log('Your data is here', data);
+    })
+    .catch ((err)=>{
+        console.log(err, "Failed!");
+    })
+
+const delayedColor = ((color, delay) =>{
+    return new Promise((resole, reject) => {
+        setTimeout(()=>{
+            document.body.style.backgroundColor = color;
+        }, delay)
+    });
 });
 
 
-const displayImages  = (shows) => {
-    for (let show of shows) {
-        if (show.show.image) {
-            const img = document.createElement('img');
-            img.src = show.show.image.medium;
-            document.body.append(img)
-        }
-    }
+delayedColor('red', 2000)
+.then (()=>{
+    delayedColor('orange', 2000)
+})
+.then (()=>{
+    delayedColor('blue', 2000)
+})
+.then (()=>{
+    delayedColor('vindigo', 2000)
+})
+.then (()=>{
+    delayedColor('green', 2000)
+})
+
+
+// async functions
+async function hello () {
+    console.log ('Hello');
 }
+
+const sing  = async () =>{
+    if(true){
+        throw new Error('oh no :(');
+    }
+    else{
+        return 'lalalalalaa'
+    }
+    
+}
+
+sing()
+.then ((data)=>{
+    console.log('Recolved: ', data);
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+const login(password, username)
