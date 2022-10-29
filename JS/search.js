756f164c842bc9f56979767e8e46f1cc77754182
Mykola <mykola.maksymov@gmail.com>
2022-10-29 19:39:24 +0200

    sorting

--- JS/search.js
+++ JS/search.js
@@ -34,7 +34,7 @@ function linearSearch(arr, number){
 
   function naiveSearch(long, short){
     let count = 0;
-    for(let i = 0; i <long.length, i++){
+    for(let i = 0; i <long.length; i++){
         for(let j = 0; j<short.length; j++){
             if(short[j]!== long[j+i]){
                 break;
---
+++ JS/sort.js
@@ -0,0 +1,35 @@
+//bubble sort 
+function bubbleSort(arr){
+    let noSwaps;
+    for (let i = arr.length; i > 0; i--) {
+        noSwaps = true;
+        for (let j = 0; j < i - 1; j++) {
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                noSwaps = false;
+            }           
+        }
+        if(noSwaps) break;       
+    }
+    return arr;
+}
+
+function selectionSort(arr){
+    let noSwaps;
+    for (let i = 0; i < arr.length; i++) {
+        let lowest = i;
+        for (let j = i+1; j < arr.length; j++) {
+            if(arr[j] < arr[lowest]){
+                lowest = j;
+              
+            }    
+        }
+        if(i !== lowest) {
+            let temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
+    }
+}
